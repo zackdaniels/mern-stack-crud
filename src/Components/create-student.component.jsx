a5dbf1f8a2fcf7389aeaf0600b5b5be4fe09792d
Zack Daniels <zackdaniels2522@gmail.com>
2022-12-15 17:33:31 -0500

    Components

---
+++ src/Components/create-student.component.jsx
@@ -0,0 +1,36 @@
+// CreateStudent Component for add new student
+  
+// Import Modules
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import StudentForm from "./StudentForm";
+
+//CreateStudent Component
+const CreateStudent = () => {
+  const [formValues, setFormValues] = 
+    useState({ name: '', email: '', rollno: '' })
+  //onSubmit handler
+  const onSubmit = studentObject => {
+    axios.post(
+'http://localhost:4000/students/create-student',
+    studentObject)
+      .then(res => {
+        if (res.status === 200)
+          alert('Student successfully created')
+        else
+          Promise.reject()
+      })
+      .catch(err => alert('Something went wrong'))
+  }
+
+  //Return student Form
+  return(
+    <StudentForm initialValues={formValues}
+      onSubmit={onSubmit}
+      enableReinitialize>
+      Create Student
+      </StudentForm>
+  )
+}
+
+export default CreateStudent
